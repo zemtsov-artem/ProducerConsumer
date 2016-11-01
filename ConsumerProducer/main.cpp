f67bd3809307ec9b7b176acaa26547808a18c132
Artem <klufone@mail.ru>
2016-11-01 13:22:30 +0300

    Initial Commit

---
+++ ConsumerProducer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ConsumerProducer
+//
+//  Created by артем on 01.11.16.
+//  Copyright © 2016 артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
